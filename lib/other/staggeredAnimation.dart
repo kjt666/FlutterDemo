d05d05dac3de77ecbf58d7ecb678f48fcdca712f
kjt <kangjiangtao@youshu.cc>
2021-09-01 10:44:45 +0800

    添加组合动画示例

---
+++ lib/other/staggeredAnimation.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class StaggeredAnimationPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return StaggeredAnimationState();
+  }
+}
+
+class StaggeredAnimationState extends State<StaggeredAnimationPage>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> opacity;
+  Animation<double> width;
+  Animation<double> height;
+  Animation<EdgeInsets> padding;
+  Animation<BorderRadius> borderRadius;
+  Animation<Color> color;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    opacity = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0.0, 0.1, curve: Curves.ease)));
+    width = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0.125, 0.25, curve: Curves.ease)));
+    height = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0.25, 0.375, curve: Curves.ease)));
+    padding = EdgeInsetsTween(begin: EdgeInsets.all(0), end: EdgeInsets.all(10))
+        .animate(CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.25, 0.375, curve: Curves.ease)));
+    borderRadius = BorderRadiusTween(
+            begin: BorderRadius.circular(4.0), end: BorderRadius.circular(75.0))
+        .animate(CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.375, 0.500, curve: Curves.ease)));
+    color = ColorTween(begin: Colors.blue[300], end: Colors.amberAccent)
+        .animate(CurvedAnimation(
+            parent: _controller, curve: Interval(0.500, 0.750)));
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StaggeredAnimation"),
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.black.withOpacity(0.1),
+              border: Border.all(color: Colors.black.withOpacity(0.5))),
+          child: AnimatedBuilder(
+            builder: _buildAnimation,
+            animation: _controller,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      width: width.value,
+      height: height.value,
+      padding: padding.value,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius.value,
+        color: color.value,
+        border: Border.all(color: Colors.indigo[300], width: 3.0),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
--- lib/other/widgetsPage.dart
+++ lib/other/widgetsPage.dart
@@ -41,6 +41,7 @@ import 'package:flutter_app/other/slider.dart';
 import 'package:flutter_app/other/sliverAppBar.dart';
 import 'package:flutter_app/other/spacer.dart';
 import 'package:flutter_app/other/stack.dart';
+import 'package:flutter_app/other/staggeredAnimation.dart';
 import 'package:flutter_app/other/text_span.dart';
 import 'package:flutter_app/other/toggleButtons.dart';
 import 'package:flutter_app/other/tweenAnimationBuilder.dart';
@@ -88,67 +89,67 @@ class _WidgetsPageState extends State<WidgetsPage> {
       body: IgnorePointer(
         ignoring: disEnable,
         child: ListView(
-          children: [
+            children: [
             getJumpItem("flexible", new FlexiblePage()),
-            getJumpItem("mediaQuery", new MediaQueryPage()),
-            getJumpItem("spacer", new SpacerPage()),
-            getJumpItem("animatedIcon(带动画的图标)", new AnimatedIconPage()),
-            getJumpItem("aspectRatio(部件宽高比)", new AspectRatioPage()),
-            getJumpItem(
-                "limitedBox(限制最大尺寸，配合列表、行、列部件使用)", new LimitedBoxPage()),
-            getJumpItem("placeHolder(占位符)", new PlaceHolderPage()),
-            getJumpItem("richText(可以展示多种文字效果)", new RichTextPage()),
-            getJumpItem("reorderableListView(可以拖动item从新排列的列表)",
-                new ReorderableListViewPage()),
-            getJumpItem("animatedSwitcher(带动画的切换)", new AnimatedSwitcherPage()),
-            getJumpItem(
-                "animatedPositioned(带动画的位置变动)", new AnimatedPositionedPage()),
-            getJumpItem("animatedPadding(带动画的边距)", new AnimatedPaddingPage()),
-            getJumpItem(
-                "indexedStack(可以展示指定下标child的部件)", new IndexedStackPage()),
-            getJumpItem("constrainedBox(限制最大最小尺寸)", new ConstrainedBoxPage()),
-            getJumpItem("stack(可以堆叠窗口的部件)", new StackPage()),
-            getJumpItem(
-                "animatedOpacity(带动画的透明度渐变)", new AnimatedOpacityPage()),
-            getJumpItem(
-                "fractionallySizeBox(百分比大小)", new FractionallySizeBoxPage()),
-            getJumpItem("selectableText(可选择的文本)", new SelectableTextPage()),
-            getJumpItem("slider(滑竿)", new SliderPage()),
-            getJumpItem("alertDialog(提醒弹窗)", new AlertDialogPage()),
-            getJumpItem(
-                "animatedCrossFade(交叉淡入淡出)", new AnimatedCrossFadePage()),
-            getJumpItem("draggableScrollableSheet(底部拖拽窗)",
-                new DraggableScrollableSheetPage()),
-            getJumpItem("colorFiltered(颜色混合)", new ColorFilteredPage()),
-            getJumpItem("toggleButtons(选项框)", new ToggleButtonsPage()),
-            getJumpItem("tweenAnimationBuilder(补间动画构建器)",
-                new TweenAnimationBuilderPage()),
-            getJumpItem("image(图片)", new ImagePage()),
-            getJumpItem("tabBar(标签栏)", new TabBarPage()),
-            getJumpItem("drawer(抽屉)", new DrawerPage()),
-            getJumpItem(
-                "listWheelScrollView(轮轴滚动列表)", new ListWheelScrollViewPage()),
-            getJumpItem("shaderMask(遮罩)", new ShaderMaskPage()),
-            // getJumpItem("builder", new BuilderPage())
-            getJumpItem("progressIndicator(进度条)", new ProgressIndicatorPage()),
-            getJumpItem("clipOval(裁剪椭圆型)", new ClipOvalPage()),
-            getJumpItem(
-                "checkBoxListTile(复选框列表item)", new CheckBoxListTilePage()),
-            getJumpItem("sliverAppBar(列表滑动折叠)", new SliverAppBarPage()),
-            getJumpItem(
-                "interactiveViewer(手势放大缩小)", new InteractiveViewerPage()),
-            getJumpItem("imageFiltered(高斯模糊)", new ImageFilteredPage()),
-            getJumpItem("physicalModel(物理模型)", new PhysicalModelPage()),
-            getJumpItem("pageView(viewPager)", new PageViewPage()),
-            getJumpItem("html(富文本)", new HtmlPage()),
-            getJumpItem("textSpan(扩展文本)", new TextSpanPage()),
-            getJumpItem("customPaint(自定义Widget)", new CustomPaintPage()),
-            getJumpItem("animation", new AnimationPage()),
-            getJumpItem("animatedWidget(动画小部件)", new AnimatedWidgetPage()),
-          ],
-        ),
+        getJumpItem("mediaQuery", new MediaQueryPage()),
+        getJumpItem("spacer", new SpacerPage()),
+        getJumpItem("animatedIcon(带动画的图标)", new AnimatedIconPage()),
+        getJumpItem("aspectRatio(部件宽高比)", new AspectRatioPage()),
+        getJumpItem(
+            "limitedBox(限制最大尺寸，配合列表、行、列部件使用)", new LimitedBoxPage()),
+        getJumpItem("placeHolder(占位符)", new PlaceHolderPage()),
+        getJumpItem("richText(可以展示多种文字效果)", new RichTextPage()),
+        getJumpItem("reorderableListView(可以拖动item从新排列的列表)",
+            new ReorderableListViewPage()),
+        getJumpItem("animatedSwitcher(带动画的切换)", new AnimatedSwitcherPage()),
+        getJumpItem(
+            "animatedPositioned(带动画的位置变动)", new AnimatedPositionedPage()),
+        getJumpItem("animatedPadding(带动画的边距)", new AnimatedPaddingPage()),
+        getJumpItem(
+            "indexedStack(可以展示指定下标child的部件)", new IndexedStackPage()),
+        getJumpItem("constrainedBox(限制最大最小尺寸)", new ConstrainedBoxPage()),
+        getJumpItem("stack(可以堆叠窗口的部件)", new StackPage()),
+        getJumpItem(
+            "animatedOpacity(带动画的透明度渐变)", new AnimatedOpacityPage()),
+        getJumpItem(
+            "fractionallySizeBox(百分比大小)", new FractionallySizeBoxPage()),
+        getJumpItem("selectableText(可选择的文本)", new SelectableTextPage()),
+        getJumpItem("slider(滑竿)", new SliderPage()),
+        getJumpItem("alertDialog(提醒弹窗)", new AlertDialogPage()),
+        getJumpItem(
+            "animatedCrossFade(交叉淡入淡出)", new AnimatedCrossFadePage()),
+        getJumpItem("draggableScrollableSheet(底部拖拽窗)",
+            new DraggableScrollableSheetPage()),
+        getJumpItem("colorFiltered(颜色混合)", new ColorFilteredPage()),
+        getJumpItem("toggleButtons(选项框)", new ToggleButtonsPage()),
+        getJumpItem("tweenAnimationBuilder(补间动画构建器)",
+            new TweenAnimationBuilderPage()),
+        getJumpItem("image(图片)", new ImagePage()),
+        getJumpItem("tabBar(标签栏)", new TabBarPage()),
+        getJumpItem("drawer(抽屉)", new DrawerPage()),
+        getJumpItem(
+            "listWheelScrollView(轮轴滚动列表)", new ListWheelScrollViewPage()),
+        getJumpItem("shaderMask(遮罩)", new ShaderMaskPage()),
+        // getJumpItem("builder", new BuilderPage())
+        getJumpItem("progressIndicator(进度条)", new ProgressIndicatorPage()),
+        getJumpItem("clipOval(裁剪椭圆型)", new ClipOvalPage()),
+        getJumpItem(
+            "checkBoxListTile(复选框列表item)", new CheckBoxListTilePage()),
+        getJumpItem("sliverAppBar(列表滑动折叠)", new SliverAppBarPage()),
+        getJumpItem(
+            "interactiveViewer(手势放大缩小)", new InteractiveViewerPage()),
+        getJumpItem("imageFiltered(高斯模糊)", new ImageFilteredPage()),
+        getJumpItem("physicalModel(物理模型)", new PhysicalModelPage()),
+        getJumpItem("pageView(viewPager)", new PageViewPage()),
+        getJumpItem("html(富文本)", new HtmlPage()),
+        getJumpItem("textSpan(扩展文本)", new TextSpanPage()),
+        getJumpItem("customPaint(自定义Widget)", new CustomPaintPage()),
+        getJumpItem("animation", new AnimationPage()),
+        getJumpItem("animatedWidget(动画小部件)", new AnimatedWidgetPage()),
+        getJumpItem("staggeredAnimation(组合动画)", new StaggeredAnimationPage())
+        ],
       ),
-    );
+    ),);
   }
 
   Widget getJumpItem(String title, Widget jumpPage) {
